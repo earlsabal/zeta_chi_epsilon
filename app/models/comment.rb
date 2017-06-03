cf3da30343500298663c109c0b9b27e41b10d107
home <home@homes-MBP-3.attlocal.net>
2017-06-03 09:49:15 -0700

    Fix seed, update comment to show readable timestamp, add logic for showing time in comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   validates :author, presence: true
   validates :body, presence: true
+
+  def posted
+  	self.created_at.strftime("%A, %d %b %Y %l:%M %p")
+  end
 end
